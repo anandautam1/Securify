0de2512e35697c06d6c5eb40004bf11545c19dbb
anandautam1 <ananda_utama@y7mail.com>
2017-08-31 12:39:44 +1000

    Login Finalized

---
+++ register.php
@@ -0,0 +1,46 @@
+<?php
+if(!empty($_GET["email"]) && !empty($_GET["firstName"]) && !empty($_GET["lastName"]) && !empty($_GET["role"]) && !empty($_GET["phoneNumber"]) && !empty($_GET["password"]))
+	{
+		$con = mysqli_connect("127.0.0.1","root","root","securify");
+
+    	if (mysqli_connect_errno($con)) {
+     		die("Failed to connect to MySQL: " . mysqli_connect_error());
+    	}
+   
+		$email = $_GET["email"];
+		
+		$firstName = $_GET["firstName"];
+		
+		$lastName = $_GET["lastName"];
+		
+		$role = $_GET["role"];
+		
+		$phoneNumber = $_GET["phoneNumber"];
+		
+		$password = $_GET["password"];
+		
+		$psalt = random_password(7);
+		$password = sha1($password.$psalt);
+
+	   $query = "insert into users (userID, fname, lname, email, phone, pass_hashed, pass_salt, Role) values (NULL, '$firstName' , '$lastName', '$email', '$phoneNumber', '$password', '$psalt', '$role')";
+	   //echo $query;
+
+	   $inserted = mysqli_query($con, $query);
+
+	   if($inserted == 1){
+	    $json['success'] = 1;
+	   }
+	   else{
+	    $json['success'] = 0;
+	   }
+	   echo json_encode($json);
+
+	   mysqli_close($con);
+	}
+
+	function random_password( $length = 7 ) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+    $password = substr( str_shuffle( $chars ), 0, $length );
+    return $password;
+	}
+?>
