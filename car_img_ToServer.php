de425bd797a3d2b3ea1981bd353461868052cbe1
anandautam1 <ananda_utama@y7mail.com>
2017-09-11 12:19:02 +1000

    PHP Securify Form Uploader

--- car_img_ToServer.php
+++ car_img_ToServer.php
@@ -1,99 +1,47 @@
 <?php
-// get password salt only 
- $password = "Bj37au9595";
- $hash = "1234567";
- $pfinal = sha1("Bj37au95951234567");
- //$password . $hash);
- //echo $pfinal; 
 
-// Login method 
-if(!empty($_GET["email"]) && empty($_GET["password"]))
-	{
-		$con = mysqli_connect("127.0.0.1","root","root","securify");
+include 'DatabaseConfig.php';
 
-		if (mysqli_connect_errno($con)) {
-	 		die("Failed to connect to MySQL: " . mysqli_connect_error());
-		}
-	
-		$email = $_GET["email"];
-		echo $email; 
-		// check if username exist first 
-    	$query = sprintf("SELECT `pass_salt` FROM `users`  WHERE email='%s'",
-              mysql_real_escape_string($email));
-    
-    	$result = $con->query($query);
-		if(!$result){
-		    // generate error message 
-		    $message  = 'Invalid query: ' . mysql_error() . "\n";
-		    // concatinate on the existing error 
-		    $message .= 'Whole query: ' . $query;
-		    die($message);
-    	}
-    	// initialzie arra y
-    	$data = array();
-    	while($row = $result->fetch_assoc()){
-      		$data[] = $row;
-    	}
-    	//var_dump($data);
+// Create connection
+$conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
+ 
+ if($_SERVER['REQUEST_METHOD'] == 'POST')
+ {
+ $DefaultId = 0;
+ 
+ $ImageData = $_POST['image_path'];
+ 
+ $ImageName = $_POST['image_name'];
 
-    	$pSalt = $data[0];
-    	$pSalt_string = $pSalt['pass_salt'];
-    	if($pSalt_string === NULL)
-    	{
-    		$json_psalt['success'] = 0;
-    		$json_psalt['pSalt'] = "NULL";
-			echo json_encode($json_psalt);
-    	}
-    	else
-    	{
-			$json_psalt['success'] = 1;
-			$json_psalt['pSalt'] = $pSalt_string;
-			echo json_encode($json_psalt);
-		}
-		mysqli_close($con);		
-	}
-else if(!empty($_GET["email"]) && !empty($_GET["password"]))
-	{
-		$con = mysqli_connect("127.0.0.1","root","root","securify");
-
-		if (mysqli_connect_errno($con)) {
-	 		die("Failed to connect to MySQL: " . mysqli_connect_error());
-		}
-	
-		$email = $_GET["email"];
-		$password = $_GET["password"];
-		// check if username exist first 
-    	$query = sprintf("SELECT `email` , `pass_hashed` FROM `users`  WHERE email='%s' and pass_hashed='%s'",
-              mysql_real_escape_string($email), mysql_real_escape_string($password));
-    
-    	$result = $con->query($query);
-		if(!$result){
-		    // generate error message 
-		    $message  = 'Invalid query: ' . mysql_error() . "\n";
-		    // concatinate on the existing error 
-		    $message .= 'Whole query: ' . $query;
-		    die($message);
-    	}
-    	// initialzie arra y
-    	$data = array();
-    	while($row = $result->fetch_assoc()){
-      		$data[] = $row;
-    	}
-    	$sanitizedData = array_filter($data);
-    	if($data === NULL || empty($sanitizedData))
-    	{
-    		$json_psalt['success'] = 0;
-			echo json_encode($json_psalt);
-    	}
-    	else
-    	{
-			$json_psalt['success'] = 1;
-			
-			echo json_encode($json_psalt);
-		}
-		mysqli_close($con);		
-	}
+ $GetOldIdSQL ="SELECT CarID FROM ImageToServerTable ORDER BY CarId ASC";
+ 
+ $Query = mysqli_query($conn,$GetOldIdSQL);
+ 
+ while($row = mysqli_fetch_array($Query)){
+ $DefaultId = $row['carID'];
+ }
+ 
+ $ImagePath = "images/$DefaultId.png";
+ 
+ $ServerURL = "https://androidjsonblog.000webhostapp.com/$ImagePath";
+ 
+ $InsertSQL = "insert into ImageToServerTable (image_path,image_name) values ('$ServerURL','$ImageName')";
+ 
 
 
-	
+ if(mysqli_query($conn, $InsertSQL)){
+
+ file_put_contents($ImagePath,base64_decode($ImageData));
+
+ $json_psalt['success'] = 1;
+ $json_psalt['message'] = "Your Image Has Been Uploaded.";
+ echo json_encode($json);
+ }
+ else{
+ $json_psalt['success'] = 0;
+ $json_psalt['message'] = "Image Upload Failed";
+ echo json_encode($json);
+ }
+ mysqli_close($conn);
+
 ?>
